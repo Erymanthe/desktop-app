918c6e96511e398e76b79397d3f8eef6832c41d3
Hinome <57831472+RealHinome@users.noreply.github.com>
2024-08-19 12:29:24 +0200

    Add components

---
+++ src/components/Button/Secondary.vue
@@ -0,0 +1,8 @@
+<template>
+    <button
+        class="rounded-md px-4 py-4 select-none text-zinc-500 dark:text-zinc-300 bg-transparent border border-zinc-200 dark:border-zinc-500 hover:text-zinc-700 dark:hover:text-zinc-200 hover:border-zinc-400 transition-colors"
+        type="button"
+    >
+        <slot />
+    </button>
+</template>
